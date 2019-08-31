f3a2258b9c6cca29b937936fee060844de4dc67b
Gustavo <gumarques@gmail.com>
2019-08-31 01:46:18 +0100

    Padroes de nomenclatura de testes

--- scr/leilao/test_avaliador.py
+++ scr/leilao/test_avaliador.py
@@ -5,7 +5,9 @@ from scr.leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 class TestAvaliador(TestCase):
 
-    def test_avalia(self):
+# Padroes de Nomenclaturas utilizados em testes:
+#       test_quando_add_em_ordem_crescente_deve_retornar_menor_e_maior_valor_de_um_lance
+    def test_deve_retornar_menor_e_maior_valor_de_um_lance_quando_add_em_ordem_crescente(self):
 
         gui = Usuario('Gui')
         ana = Usuario('Ana')
@@ -17,6 +19,28 @@ class TestAvaliador(TestCase):
 
         leilao.lances.append(lance_da_ana)
         leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 10.0
+        maior_valor_esperado = 156.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_menor_e_maior_valor_de_um_lance_quando_add_em_ordem_decrescente(self):
+
+        gui = Usuario('Gui')
+        ana = Usuario('Ana')
+
+        lance_da_ana = Lance(ana, 10.0)
+        lance_do_gui = Lance(gui, 156.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_da_ana)
 
         avaliador = Avaliador()
         avaliador.avalia(leilao)
